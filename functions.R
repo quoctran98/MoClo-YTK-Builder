e256cf34111ff836e983b3add4cd62189e0a2fda
Quoc Tran <quoctran1998@gmail.com>
2019-08-07 00:32:41 -0700

    Biostrings

--- functions.R
+++ functions.R
@@ -52,3 +52,22 @@ molesToMass <- function (moles, length, molePrefix = 0.000000000000001, massPref
   mass <- (moles * molePrefix) * ((length * avgWeight) + endWeight)
   return(mass / massPrefix)
 }
+
+assembleCassette <- function (t1,t2,t3,t3a,t3b,t4,t4a,t4b,t5,t6,t7,t8,t8a,t8b,t234,t678) {
+  t1DNA <- (parts[parts$description == t1 & parts$type == "1","sequence"])
+  t2DNA <- (parts[parts$description == t2 & parts$type == "2","sequence"])
+  t3DNA <- (parts[parts$description == t3 & parts$type == "3","sequence"])
+  t3aDNA <- (parts[parts$description == t3a & parts$type == "3a","sequence"])
+  t3bDNA <- (parts[parts$description == t3b & parts$type == "3b","sequence"])
+  t4DNA <- (parts[parts$description == t4 & parts$type == "4","sequence"])
+  t4aDNA <- (parts[parts$description == t4a & parts$type == "4a","sequence"])
+  t4bDNA <- (parts[parts$description == t4b & parts$type == "4b","sequence"])
+  t5DNA <- (parts[parts$description == t5 & parts$type == "5","sequence"])
+  t6DNA <- (parts[parts$description == t6 & parts$type == "6","sequence"])
+  t7DNA <- (parts[parts$description == t7 & parts$type == "7","sequence"])
+  t8DNA <- (parts[parts$description == t8 & parts$type == "8","sequence"])
+  t8aDNA <- (parts[parts$description == t8a & parts$type == "8a","sequence"])
+  t8bDNA <- (parts[parts$description == t8b & parts$type == "8b","sequence"])
+  t234DNA <- (parts[parts$description == t234 & parts$type == "234","sequence"])
+  t678DNA <- (parts[parts$description == t678 & parts$type == "678","sequence"])
+}
--- server.R
+++ server.R
@@ -2,9 +2,11 @@ library(shiny)
 library(DT)
 library(stringr)
 library(shinyjs)
+library(Biostrings)
 
 parts <- read.csv("data/parts.csv", row.names = 1)
 parts <- rbind(parts, read.csv("data/user.csv", row.names = 1))
+parts[,"sequence"] <- DNAString(parts[,"sequence"])
 stock <- read.csv("data/stock.csv", row.names = 1)
 stock$concentration <- (rnorm(96, mean = 150, sd = 50)) #random test data
 
